2d39f119774ca6297806433ea3bcb94b68b0ce85
Ma'mur Mulyawan <mamurmulya@gmail.com>
2024-07-12 14:20:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LaptopController;
 
-Route::resource('/', LaptopController::class);
+Route::get('/', function(){
+	return redirect('/laptop');
+});
+
+Route::resource('/laptop', LaptopController::class);
+Route::delete('/laptop/hapusFitur/{id}', [LaptopController::class, 'hapusFitur']);
