177ab256691a00d9c5aa5aea03234afc30b1430e
Ma'mur Mulyawan <mamurmulya@gmail.com>
2024-07-12 14:20:34 +0700

    Update tambah.blade.php

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -2,56 +2,56 @@
 
 @section('content')
 <div class="container mt-5">
-		<h1 class="text-center my-3">Multiple Input dengan Gambar</h1>
-		<div class="row">
-			<div class="col-md-12">
-				<div class="card">
-					<div class="card-header">
-						<a href="/" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
-					</div>
-					<div class="card-body">
-						<form action="/store" method="post" enctype="multipart/form-data">
-							@csrf
-							@method('POST')
-							<div class="row">
-								<div class="col-md-4 mb-2">
-									<label for="merk">Merk Laptop</label>
-									<input type="text" name="merk" class="form-control" required>
-								</div>
-								<div class="col-md-4 mb-2">
-									<label for="gambar">Gambar Laptop</label>
-									<input type="file" name="gambar" class="form-control" required>
-								</div>
-								<div class="col-md-4 mb-2">
-									<label for="harga">Harga Laptop</label>
-									<input type="text" name="harga" class="form-control" required>
-								</div>
-								<div class="col-md-12 mt-3">
-									<div class="card">
-										<div class="card-header">
-											<button class="btn btn-success" type="button" onclick="tambahFitur()">Tambah Fitur</button>
-										</div>
-										<div class="card-body">
-											<div class="row" id="formFitur">
-	                                            <div class="col-md-10">
-	                                                <input type="text" class="form-control mb-2" name="fields[0][fitur]" placeholder="Nama Fitur" required>
-	                                            </div>
-	                                            <div class="col-md-2">
-	                                            </div>
-	                                        </div>
+	<h1 class="text-center my-3">Multiple Input dengan Gambar</h1>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					<a href="/laptop" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
+				</div>
+				<div class="card-body">
+					<form action="/laptop" method="post" enctype="multipart/form-data">
+						@csrf
+						@method('POST')
+						<div class="row">
+							<div class="col-md-4 mb-2">
+								<label for="merk">Merk Laptop</label>
+								<input type="text" name="merk" class="form-control" required>
+							</div>
+							<div class="col-md-4 mb-2">
+								<label for="gambar">Gambar Laptop</label>
+								<input type="file" name="gambar" class="form-control" required>
+							</div>
+							<div class="col-md-4 mb-2">
+								<label for="harga">Harga Laptop</label>
+								<input type="text" name="harga" class="form-control" required>
+							</div>
+							<div class="col-md-12 mt-3">
+								<div class="card">
+									<div class="card-header">
+										<button class="btn btn-success" type="button" onclick="tambahFitur()">Tambah Fitur</button>
+									</div>
+									<div class="card-body">
+										<div class="row" id="formFitur">
+											<div class="col-md-10">
+												<input type="text" class="form-control mb-2" name="fields[0][fitur]" placeholder="Nama Fitur" required>
+											</div>
+											<div class="col-md-2">
+											</div>
 										</div>
 									</div>
 								</div>
-								<div class="col-md-12 mt-3">
-									<button class="btn btn-primary" type="submit">Submit</button>
-								</div>
 							</div>
-						</form>
-					</div>
+							<div class="col-md-12 mt-3">
+								<button class="btn btn-primary" type="submit">Submit</button>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
 
 @push('script')
