96673548b8daf49ff1a60704c9b52bce6a42ea86
Ma'mur Mulyawan <mamurmulya@gmail.com>
2024-07-12 11:48:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaptopController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', LaptopController::class);
